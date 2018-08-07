f370f4b88186c1460d96bbef4f82b7acda26765d
Daniel Medrano <bestenglishking@hotmail.com>
2018-08-07 12:57:13 -0500

    Agregar manejo de excepciones

--- SizeFiles.java
+++ SizeFiles.java
@@ -3,26 +3,43 @@ import java.util.Arrays;
 
 public class SizeFiles {
     public static void main(String[] args) {
-            String path = "\\\\172.18.48.230\\nice_sc\\niceapps1din\\48\\2018\\8\\5\\SC_SC Dinamarca 1";
-            String[] lines = GetFileLines(path);
-            int total = 0;
+        String path = "\\\\172.18.48.230\\nice_sc\\niceapps1din\\48\\2018\\8\\5\\SC_SC Dinamarca 1";
+        String[] lines = GetFileLines(path);
+        int total = 0;
 
-            for (int i = 0; i <= lines.length -1; i++) {
-                String fileName = path + "\\" + lines[i];
-                int lineCount = GetFileLines(fileName).length;
+        for (int i = 0; i <= lines.length -1; i++) {
+            String fileName = path + "\\" + lines[i];
+            int lineCount = GetFileLines(fileName).length;
 
-                System.out.println(fileName);
-                System.out.println(lineCount);
-                total += lineCount;
-            }
+            System.out.println(fileName);
+            System.out.println(lineCount);
+            total += lineCount;
+        }
 
-            System.out.println();
-            System.out.println("Total: " + total);
+        System.out.print("\nTotal: " + total);
     }
 
     public static String[] GetFileLines(String filePath) {
-            File fichero = new File(filePath);
+        File fichero;
+        String[] lines;
 
-            return fichero.list();
+        try {
+            fichero = new File(filePath);
+            lines = fichero.list();
+
+            if (lines != null)
+                return lines;
+            else
+                return new String[];
+        }
+        catch (NullPointerException e) {
+            System.out.print("\nError: la ruta indicada no existe.");
+        }
+        catch (SecurityException e) {
+            System.out.print("\nError: seguridad");
+        }
+        catch (Exception e) {
+            System.out.print("\nError: algo salió mal :v");
+        }
     }
 }
