82d3dd43b24d008c209694663e767cc6545b6799
Emmanuel Pérez <emmanuel.arreolap@mx.atento.com>
2018-08-07 13:28:32 -0500

    Simplificar y renombrar metodos

--- SizeFiles.java
+++ SizeFiles.java
@@ -1,15 +1,16 @@
 import java.io.File;
 import java.util.Arrays;
+import javax.swing.*;
 
 public class SizeFiles {
     public static void main(String[] args) {
         String path = "\\\\172.18.48.230\\nice_sc\\niceapps1din\\48\\2018\\8\\5\\SC_SC Dinamarca 1";
-        String[] lines = GetFileLines(path);
+        String[] lines = GetDirContents(path);
         int total = 0;
 
         for (int i = 0; i <= lines.length -1; i++) {
             String fileName = path + "\\" + lines[i];
-            int lineCount = GetFileLines(fileName).length;
+            int lineCount = lines.length;
 
             System.out.println(fileName);
             System.out.println(lineCount);
@@ -19,27 +20,13 @@ public class SizeFiles {
         System.out.print("\nTotal: " + total);
     }
 
-    public static String[] GetFileLines(String filePath) {
-        File fichero;
-        String[] lines;
+    private static String[] GetDirContents(String filePath) {
+        File fichero = new File(filePath);
+        String[] lines = fichero.list();
 
-        try {
-            fichero = new File(filePath);
-            lines = fichero.list();
-
-            if (lines != null)
-                return lines;
-            else
-                return new String[];
-        }
-        catch (NullPointerException e) {
-            System.out.print("\nError: la ruta indicada no existe.");
-        }
-        catch (SecurityException e) {
-            System.out.print("\nError: seguridad");
-        }
-        catch (Exception e) {
-            System.out.print("\nError: algo salió mal :v");
-        }
+        if (lines != null)
+            return lines;
+        else
+            return new String[0];
     }
 }
