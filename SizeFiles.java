6104e7f892830f543ffd5bbd55f64e9fb50e88e7
Daniel Medrano <bestenglishking@hotmail.com>
2018-08-07 12:44:17 -0500

    Correcciones 'menores'

--- SizeFiles.java
+++ SizeFiles.java
@@ -2,44 +2,27 @@ import java.io.File;
 import java.util.Arrays;
 
 public class SizeFiles {
-	public static void main(String[] args) {
-			//String path = "\\\\172.18.19.141\\nice\\Grabaciones\\Llamadas nice";
-		    //System.out.println(Size(path));
-			//System.out.println(/*Arrays.toString*/(nameCarpet(path)[1]));
-			String path = "\\\\172.18.48.230\\nice_sc\\niceapps1din\\48\\2018\\8\\5\\SC_SC Dinamarca 1";
-			String[] as = Size(nameCarpet(path), path);
-			int fSize = 0;
-			for (int i = 0; i <= nameCarpet(path).length -1; i++) {
-				String path2 = "\\\\172.18.48.230\\nice_sc\\niceapps1din\\48\\2018\\8\\5\\SC_SC Dinamarca 1" + "\\" + as[i];
-				System.out.println(path2);
-				int x = nameCarpet(path2).length;
-				System.out.println(x);
-				fSize += x;
-			}
-			System.out.println(fSize);
-			//String path2 = "\\\\172.18.48.230\\nice_sc\\niceapps1din\\48\\2018\\8\\5\\SC_SC Dinamarca 1" + "\\" + as[1];
-			//System.out.println(path2);
-			//int x = nameCarpet(path2).length;
-			//System.out.println(x);
+    public static void main(String[] args) {
+            String path = "\\\\172.18.48.230\\nice_sc\\niceapps1din\\48\\2018\\8\\5\\SC_SC Dinamarca 1";
+            String[] lines = GetFileLines(path);
+            int total = 0;
 
-			//for (int i = 0; i <= nameCarpet(path).length - 1; i ++ ) {
-			//	System.out.println("File No. = " + (i + 1) + " : " + as[i]);
-			//}
-			//System.out.println(as[0]);
-	}
+            for (int i = 0; i <= lines.length -1; i++) {
+                String fileName = path + "\\" + lines[i];
+                int lineCount = GetFileLines(fileName).length;
 
-	public static String[] nameCarpet(String path) {
-			String sTest = path;
-    		File fichero = new File(sTest);
-    		String as[] = fichero.list();
-    		return as;
-   			//return fichero.list() /*.length*/;
-	}
+                System.out.println(fileName);
+                System.out.println(lineCount);
+                total += lineCount;
+            }
 
-	public static String[] Size(String[] nameCarpet, String path) {
-		String[] x = new String[nameCarpet(path).length];
-		x = nameCarpet(path);
-		//x[0] = nameCarpet(path)[1];
-		return x;
-	}
+            System.out.println();
+            System.out.println("Total: " + total);
+    }
+
+    public static String[] GetFileLines(String filePath) {
+            File fichero = new File(filePath);
+
+            return fichero.list();
+    }
 }
