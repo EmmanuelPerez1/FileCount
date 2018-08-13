5a2f383935af4fdb3a173a813e5165f1ea41ea12
Emmanuel.Perez1 <emmanuel.arreola@ahksystems.com>
2018-08-13 08:06:32 -0500

    "Método get count añadido"

--- SizeFiles.java
+++ SizeFiles.java
@@ -1,23 +1,11 @@
 import java.io.File;
 import java.util.Arrays;
-import javax.swing.*;
+//import javax.swing.*;
 
 public class SizeFiles {
     public static void main(String[] args) {
         String path = "\\\\172.18.48.230\\nice_sc\\niceapps1din\\48\\2018\\8\\5\\SC_SC Dinamarca 1";
-        String[] lines = GetDirContents(path);
-        int total = 0;
-
-        for (int i = 0; i <= lines.length -1; i++) {
-            String fileName = path + "\\" + lines[i];
-            int lineCount = lines.length;
-
-            System.out.println(fileName);
-            System.out.println(lineCount);
-            total += lineCount;
-        }
-
-        System.out.print("\nTotal: " + total);
+        GetCount(path);
     }
 
     private static String[] GetDirContents(String filePath) {
@@ -28,5 +16,22 @@ public class SizeFiles {
             return lines;
         else
             return new String[0];
+    }
+
+    private static void GetCount(String path1){
+        String[] lines = GetDirContents(path1);
+        int total = 0;
+
+        for (int i = 0; i <= lines.length -1; i++) {
+            String fileName = path1 + "\\" + lines[i];
+            String[] filecount = GetDirContents(fileName);
+            int lineCount = filecount.length;
+
+            System.out.println(fileName);
+            System.out.println(lineCount);
+            total += lineCount;
+        }
+
+        System.out.print("\nTotal: " + total);
     }
 }
