7c1b0c2c0e48ca84516ea5250583c8fdcea9e698
EmmanuelPerez1 <emmanuel.arreola@ahksystem.com>
2018-08-13 13:00:22 -0500

    "Método get count añadido"

--- SizeFiles.java
+++ SizeFiles.java
@@ -1,24 +1,24 @@
 import java.io.File;
-import java.util.Arrays;
-//import javax.swing.*;
+import java.util.TimeZone;
+import java.util.Calendar;
 
 public class SizeFiles {
     public static void main(String[] args) {
-        String path = "\\\\172.18.48.230\\nice_sc\\niceapps1din\\48\\2018\\8\\5\\SC_SC Dinamarca 1";
-        GetCount(path);
+        //String path = "\\\\172.18.48.230\\nice_sc\\niceapps1din\\48\\2018\\8\\5\\SC_SC Dinamarca 1";
+        System.out.print("De la siguiente ruta: " + GetDir() + " hay este numero de grabaciones:  " +  GetCount(GetDir()) );
     }
 
     private static String[] GetDirContents(String filePath) {
-        File fichero = new File(filePath);
+        File fichero = new File(String.valueOf(filePath));
         String[] lines = fichero.list();
 
-        if (lines != null)
+        if (lines != null) {
             return lines;
-        else
-            return new String[0];
+        }
+        return new String[0];
     }
 
-    private static void GetCount(String path1){
+    private static int GetCount(String path1){
         String[] lines = GetDirContents(path1);
         int total = 0;
 
@@ -27,11 +27,18 @@ public class SizeFiles {
             String[] filecount = GetDirContents(fileName);
             int lineCount = filecount.length;
 
-            System.out.println(fileName);
-            System.out.println(lineCount);
             total += lineCount;
         }
 
-        System.out.print("\nTotal: " + total);
+        return total;
+    }
+
+    private static String GetDir(){
+        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+        int day = calendar.get(Calendar.DATE) - 1;
+        int month = calendar.get(Calendar.MONTH) + 1;
+        //System.out.printf("Día: " + (day - 1) + " mes " + (month + 1));
+        String path = "\\\\172.18.48.230\\nice_sc\\niceapps1din\\48\\2018\\"+month+"\\"+day+ "\\SC_SC Dinamarca 1";
+        return path;
     }
 }
