950a226c24beb093ac0705d4d8165c28b3559e55
Daniel Medrano <bestenglishking@hotmail.com>
2018-08-16 17:52:11 -0500

    Ejemplo de funcionamiento

--- SizeFiles.java
+++ SizeFiles.java
@@ -1,17 +1,15 @@
-import java.util.ArrayList;
-
 public class SizeFiles {
     public static void main(String[] args) {
-        String path = "\\\\172.18.48.230\\nice_sc\\niceapps1din\\48";
-        ArrayList<StorageData> arrg = StorageData.getStorageSet(new String[]{path});
+        String[] paths = new String[]{
+                "\\\\172.18.48.230\\nice_sc\\niceapps1din\\48",
+                "\\\\172.18.104.119\\Amex\\niceapps1sev\\46"};
+        StorageData[] sdArray = StorageData.get(paths),
+                sdArray1;
+        String result = Json.encode(sdArray);
 
-        for (StorageData sd : arrg)
-            System.out.println(sd.toString());
-        /*for (int i = 0; i <= GetDir().length - 1; i++) {
-            System.out.print("De la siguiente ruta: " + GetDir()[i] + " hay este número de grabaciones:  " + GetCount(GetDir()[i]) + "\n");
-            //StorageData sd = new StorageData();
-*/
+        System.out.println(result);
+        sdArray1 = Json.decode(result);
+
+        System.out.println(sdArray1.toString());
     }
 }
-
-
