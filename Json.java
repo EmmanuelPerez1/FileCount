5e17924485b9cd1cf90fadf7b1eb79b0a2bc7e8a
Daniel Medrano <bestenglishking@hotmail.com>
2018-08-16 19:46:20 -0500

    Agregar documentación pedorra

--- Json.java
+++ Json.java
@@ -5,6 +5,13 @@ import com.google.gson.JsonParser;
 import java.util.ArrayList;
 
 public class Json {
+    /**
+     * Returns a single-line String JSON.
+     *
+     * @param storageDataArray an array of StorageData containing the
+     *                         information to parse to JSON
+     * @return String object representation of the StorageData array
+     */
     public static String encode(StorageData[] storageDataArray) {
         ArrayList<StorageData> storageDataList = new ArrayList<>();
         StringBuilder stringBuilder = new StringBuilder();
@@ -19,6 +26,11 @@ public class Json {
         return stringBuilder.toString();
     }
 
+    /**
+     * Parses a stringified JSON String object to a StorageData array
+     * @param json a stringified JSON String object
+     * @return array of StorageData
+     */
     public static StorageData[] decode(String json) {
         ArrayList<StorageData> arrayList = new ArrayList<>();
         JsonArray jsonArray = new JsonParser().parse(json)
